53ce1c5d4984092c62dd6a85009ea228876bdbbd
Kent Hervey <41525831+kent-hervey@users.noreply.github.com>
2020-09-09 15:22:47 -0500

    first commit

---
+++ src/com/hervey/app/DrawBoxes3.java
@@ -0,0 +1,36 @@
+package com.hervey.app;
+
+public class DrawBoxes3 {
+
+	public static void main(String[] args) {
+		DrawBoxes3.drawbox(); //Note that Class designation DrawBoxes3 is optional because we are in that class
+		System.out.println();
+		DrawBoxes3.drawbox();
+		System.out.println();
+		drawbox();
+		
+		//More fun below
+		//let's draw three boxes:
+		DrawBoxesWithParameters.drawSpecifiedNumberOfBoxes(3); //I created this method before it existed, then let Eclipse create the method header automatically
+		
+		//Let's draw 2 boxes with width 8, and height of 4
+		//Note:  height and width must be at least two, testing for this would make the methods more complicated
+		DrawBoxesWithParameters.drawNumberWidthHeight(2, 8, 4);
+		DrawBoxesWithParameters.drawNumberWidthHeight(2, 32, 12);
+		
+		
+
+	}
+
+	public static void drawbox() {
+		System.out.println("+-------+");
+		System.out.println("|       |");
+		System.out.println("|       |");		
+		System.out.println("+-------+");			
+	}
+	
+	
+	
+	
+
+}
---
+++ src/com/hervey/app/DrawBoxesWithParameters.java
@@ -0,0 +1,55 @@
+package com.hervey.app;
+
+public class DrawBoxesWithParameters {
+	//When lines are too long to display on Eclipse, using Alt-Shift-Y will toggle Word Wrap
+	public static void drawSpecifiedNumberOfBoxes(int i) { //i is not a self documenting variable name, but it was automatic, so I left it for demonstration purposes.  numBoxes, or numberOfBoxes would be better
+		System.out.println("Now we will draw this many boxes: " + i);
+		for(int j =0; j<i; j++) {
+			DrawBoxes3.drawbox();
+			
+		}
+	}
+	
+	public static void drawBoxesThisHeightThisWidth(int height, int width) {
+		//top and bottom or the same, so they could drawn using another method and called twice in this method
+		//draw top
+		System.out.print("+");
+		for(int i=0; i<width-2; i++) {
+			System.out.print("-");
+		}
+		System.out.print("+\n");
+		
+		//draw middle
+
+		for(int i=0; i<height-2; i++) {
+			System.out.print("|");
+			for(int j=0; j<width-2; j++) {
+				System.out.print(" ");
+			}
+			System.out.print("|\n");
+		}
+
+		
+		
+		//draw bottom
+		System.out.print("+");
+		for(int i=0; i<width-2; i++) {
+			System.out.print("-");
+		}
+		System.out.print("+\n");
+		
+		
+	}
+
+	public static void drawNumberWidthHeight(int quantity, int width, int height) {
+		System.out.println("Now we will draw this many boxes: " + quantity + " ,with this width and height:  " + width + " , " +height);
+		for(int j =0; j<quantity; j++) {
+			drawBoxesThisHeightThisWidth(height, width);
+			System.out.print("\n");
+		}
+		
+	}
+	
+	
+
+}
